7743c16c2a152e5d97b7046a7cad827de608654f
Milo Chen <milo_chen@mail2000.com.tw>
2019-02-02 16:05:25 +0000

    Add some information for eosioPrivateKey.sh

--- nodeos/eosioPrivateKey.sh
+++ nodeos/eosioPrivateKey.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 EOSIO_PRIVATE_KEY=5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3
+# WHAT ?! Open private key ???
+# Don't worry about it.
+# The private key has opened by Started Guide of EOS. Everybody knew it. 
+# It is development key for eosio account.
+# If you want to use this project in another eosio chain, you can change EOSIO_PRIVATE_KEY by your self in your personal purpose.
+# Right now, the eos-script is easily for you to go through EOS started guide. 
+
+
