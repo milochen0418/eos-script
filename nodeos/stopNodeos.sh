3d61c33061b5a69cbce57447f67a8db208c98e16
Milo Chen <milo_chen@mail2000.com.tw>
2019-02-06 07:02:00 +0000

    ./stopNodeos.sh and stopNodeos.sh have different meaning. when youcall stopNodeos.sh , then process  will kill itself if you don't set grep more like eosio

--- nodeos/stopNodeos.sh
+++ nodeos/stopNodeos.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-for i in $(ps -ef | grep nodeos | awk '{print $2}');
-	do kill -9 $i; 
+for i in $(ps -ef | grep nodeos | grep eosio | awk '{print $2}')
+do 
+	kill -9 $i; 
 done
 rm -rf ~/eos
