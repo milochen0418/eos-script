7dd036f63499f3ed4202e7c13ccabbe2217853dc
Milo Chen <milo_chen@mail2000.com.tw>
2019-02-13 13:54:43 +0000

    support Ubuntu 18.04 installation for new version of eosio. I refer eos cardgame docker setting

--- installation/installCDT.sh
+++ installation/installCDT.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
-if [ ! -e eosio.cdt-1.4.1.x86_64.deb ]; then 
-	wget https://github.com/EOSIO/eosio.cdt/releases/download/v1.4.1/eosio.cdt-1.4.1.x86_64.deb
+
+
+UBUNTU_VER=$(lsb_release -a | grep Release | awk '{print $2}')
+
+if [ $UBUNTU_VER = "16.04" ]; then
+	if [ ! -e eosio.cdt-1.4.1.x86_64.deb ]; then 
+		wget https://github.com/EOSIO/eosio.cdt/releases/download/v1.4.1/eosio.cdt-1.4.1.x86_64.deb
+	fi
+	#wget https://github.com/EOSIO/eosio.cdt/releases/download/v1.4.1/eosio.cdt-1.4.1.x86_64.deb
+	sudo apt install -y ./eosio.cdt-1.4.1.x86_64.deb
+	exit 0;
 fi
-#wget https://github.com/EOSIO/eosio.cdt/releases/download/v1.4.1/eosio.cdt-1.4.1.x86_64.deb
-sudo apt install -y ./eosio.cdt-1.4.1.x86_64.deb
+
+
+if [ $UBUNTU_VER = "18.04" ]; then
+	if [ ! -e eosio.cdt-1.5.0-1_amd64.deb ]; then 
+		#wget https://github.com/EOSIO/eosio.cdt/releases/download/v1.4.1/eosio.cdt-1.4.1.x86_64.deb
+		wget https://github.com/EOSIO/eosio.cdt/releases/download/v1.5.0/eosio.cdt_1.5.0-1_amd64.deb
+	fi
+	#wget https://github.com/EOSIO/eosio.cdt/releases/download/v1.4.1/eosio.cdt-1.4.1.x86_64.deb
+	sudo apt install -y ./eosio.cdt-1.5.0-1_amd64.deb
+	exit 0;
+fi
+
+
+
--- installation/installEosio.sh
+++ installation/installEosio.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
+#if [ $(lsb_release -a | grep Release | awk '{print $2}' ) = "16.04" ]; then echo YES; fi
 
-if [ ! -e eosio_1.5.0-1-ubuntu-16.04_amd64.deb ]; then 
-	wget https://github.com/eosio/eos/releases/download/v1.5.0/eosio_1.5.0-1-ubuntu-16.04_amd64.deb
+UBUNTU_VER=$(lsb_release -a | grep Release | awk '{print $2}')
+
+if [ $UBUNTU_VER = "16.04" ]; then
+	if [ ! -e eosio_1.5.0-1-ubuntu-16.04_amd64.deb ]; then 
+		wget https://github.com/eosio/eos/releases/download/v1.5.0/eosio_1.5.0-1-ubuntu-16.04_amd64.deb
+	fi
+	#wget https://github.com/eosio/eos/releases/download/v1.5.0/eosio_1.5.0-1-ubuntu-16.04_amd64.deb
+	sudo apt install -y ./eosio_1.5.0-1-ubuntu-16.04_amd64.deb
+	exit 0
 fi
-#wget https://github.com/eosio/eos/releases/download/v1.5.0/eosio_1.5.0-1-ubuntu-16.04_amd64.deb
-sudo apt install -y ./eosio_1.5.0-1-ubuntu-16.04_amd64.deb
+
+
+if [ $UBUNTU_VER = "18.04" ]; then
+	if [ ! -e eosio_1.6.0-1-ubuntu-18.04_amd64.deb ]; then 
+		wget https://github.com/EOSIO/eos/releases/download/v1.6.0/eosio_1.6.0-1-ubuntu-18.04_amd64.deb
+	fi
+	sudo apt install -y ./eosio_1.6.0-1-ubuntu-18.04_amd64.deb
+	exit 0
+fi
+
+echo "eosio package install failed because eos-script only support for Ubuntu 16.04 and Ubuntu 18.04"
+exit 1;
